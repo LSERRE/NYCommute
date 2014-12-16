5758c3ef97c4180286aa2d2dc3d9fccf889643ed
Louis Serre <louis.serre@gmail.com>
2014-12-16 13:27:43 +0100

    Initial Commit

---
+++ NYCommute/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYCommute
+//
+//  Created by Louis SERRE on 16/12/2014.
+//  Copyright (c) 2014 EQUIPE_11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
