0c2c43f9fc3718e63d78de8abe5955f72774455e
Loïc Saint-Roch <hack@loic.io>
2014-12-16 14:56:56 +0100

    Try importing Parse on a Swift project

---
+++ NYTimesCommute/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYTimesCommute
+//
+//  Created by Loïc Saint-Roch on 12/16/14.
+//  Copyright (c) 2014 loicsaintroch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
